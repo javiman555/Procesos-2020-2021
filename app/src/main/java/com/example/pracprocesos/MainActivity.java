1f168d47c17726168d1701698e4ac0d7f90a52a9
CarlotaMenendez <49288159+CarlotaMenendez@users.noreply.github.com>
2020-11-01 10:12:43 +0100

    Botón de apagado

--- app/src/main/java/com/example/pracprocesos/MainActivity.java
+++ app/src/main/java/com/example/pracprocesos/MainActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView ivBoton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        ivBoton = (ImageView) findViewById(R.id.ivBoton);
+
+        ivBoton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){
+                finish();
+            }
+        });
     }
 }
