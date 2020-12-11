a61b72b5aec95c594a07e8167be5ea93373c8fec
mmartinal <49288214+mmartinal@users.noreply.github.com>
2020-12-11 11:46:12 +0100

    ajustes

--- app/src/main/java/com/example/pracprocesos/MainActivity.java
+++ app/src/main/java/com/example/pracprocesos/MainActivity.java
@@ -3,17 +3,22 @@ package com.example.pracprocesos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+
+import java.nio.channels.SeekableByteChannel;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,8 +33,10 @@ public class MainActivity extends AppCompatActivity {
     TextView txtView;
     MediaPlayer mp;
     MediaPlayer mp2;
-    Switch Sonido;
+    private AudioManager audioManager;
+    private SeekBar vol;
     ToggleButton Musica;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,13 +50,14 @@ public class MainActivity extends AppCompatActivity {
         botonInfo =  (ImageView) findViewById(R.id.botonInfo);
         botonJugar = (Button) findViewById(R.id.botonjugar);
         botonhelp = (Button) findViewById(R.id.botonhelp);
-        Sonido = (Switch) findViewById(R.id.sonido);
         Musica = (ToggleButton) findViewById(R.id.musica);
         mp = MediaPlayer.create(this, R.raw.sonidoboton);
         mp2 = MediaPlayer.create(this, R.raw.sonidofondo);
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        initControls();
 
 
-        //mp2.setLooping(true);
+        mp2.setLooping(true);
         mp2.start();
         ivBoton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
@@ -60,37 +68,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v){
                 mp.start();
                 startActivity(new Intent(MainActivity.this,ajustes_popup.class));
+
             }
         });
-        Musica.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v){
-                if (Musica.isChecked()) {
-                    mp2.start();
-                } else {
-                    mp2.stop();
-                }
-            }
-        });
-
-
-        /*ajustesFondo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ajustesFondo.setVisibility(View.INVISIBLE);
-            }
-        });*/
         botonInfo.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
                 mp.start();
                 startActivity(new Intent(MainActivity.this,info_popup.class));
             }
         });
-        /*fondoinfo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fondoinfo.setVisibility(View.INVISIBLE);
-            }
-        });*/
+
         botonhelp.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
                 mp.start();
@@ -109,13 +96,39 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    public void initControls()
+    {
+        try{
+            vol = (SeekBar) findViewById(R.id.volumen);
+            audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+            vol.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+            vol.setProgress((audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)));
+            vol.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
 
-    /*public void onClick(View src) {
-        if(Musica.isChecked()) {
-            mp2.start();
+                public void onStopTrackingTouch(SeekBar seekBar){
+
+                }
+                public void onStartTrackingTouch(SeekBar seekBar){
+
+                }
+                public void onProgressChanged(SeekBar seekBar,int progress, boolean fromUser){
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        else{
-            mp2.stop();
-        }
+    }
+    /*public void onPause() {
+        mp2.stop();
+        mp2.release();
+        super.onPause();
+    }
+
+    public void onResume(){
+        mp2 = MediaPlayer.create(this, R.raw.sonidofondo);
+        mp2.setLooping(false);
+        mp2.start();
+        super.onResume();
     }*/
 }
